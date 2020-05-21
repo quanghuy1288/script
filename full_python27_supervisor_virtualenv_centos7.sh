f1d637840a21cd8c3efc5d7ca70ad78538a67cc9
Võ Quang Huy <huyquv@gmail.com>
2020-05-21 13:28:48 +0700

    Create full_python27_supervisor_virtualenv_centos7.sh

---
+++ full_python27_supervisor_virtualenv_centos7.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+sudo yum groupinstall -y development
+sudo yum install -y zlib-dev openssl-devel sqlite-devel bzip2-devel   wget xz-libs
+sudo wget http://www.python.org/ftp/python/2.7.16/Python-2.7.16.tar.xz
+sudo xz -d Python-2.7.16.tar.xz
+sudo tar -xvf Python-2.7.16.tar
+cd Python-2.7.16
+sudo ./configure --prefix=/usr/local
+sudo make && make altinstall
+
+/usr/local/bin/pip install virtualenv
+
+/usr/local/bin/pip install supervisor
+ln -s /usr/local/bin/supervisord /bin/supervisord
+ln -s /usr/local/bin/supervisorctl /bin/supervisorctl
+
+mkdir -p /etc/supervisord/conf.d
+/usr/local/bin/echo_supervisord_conf > /etc/supervisord/supervisord.conf
+echo "[include]" >> /etc/supervisord/supervisord.conf
+echo "files = conf.d/*.ini" >> /etc/supervisord/supervisord.conf
+
